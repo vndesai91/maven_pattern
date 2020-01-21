5f716cacee805761d12eaa03823d2abed421c4ae
desai <desai@123.com>
2020-01-21 19:24:02 +0530

    pattern maven

---
+++ src/test/java/project1/patterntest.java
@@ -0,0 +1,14 @@
+package project1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class patterntest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
